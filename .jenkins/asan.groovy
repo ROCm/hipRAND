03a54bc34214851ce17aed18ef425b9cefb4cd5d
thansen-amd <Tegan.Hansen@amd.com>
2023-11-27 16:03:10 -0700

    ASAN Setup and switch to settings map

--- .jenkins/asan.groovy
+++ .jenkins/asan.groovy
@@ -8,8 +8,7 @@ def runCI =
 {
     nodeDetails, jobName->
     
-    def prj = new rocProject('hipRAND', 'PreCheckin')
-    prj.libraryDependencies = ['rocRAND']
+    def prj = new rocProject('hipRAND', 'address-sanitizer')
 
     def nodes = new dockerNodes(nodeDetails, jobName, prj)
 
@@ -17,7 +16,7 @@ def runCI =
 
     boolean formatCheck = false
 
-    def settings = [staticLibrary: true]
+    def settings = [addressSanitizer: true]
      
     def compileCommand =
     {
@@ -26,6 +25,7 @@ def runCI =
         commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/common.groovy"
         commonGroovy.runCompileCommand(platform, project, jobName, settings)
     }
+
     
     def testCommand =
     {
@@ -47,10 +47,10 @@ def runCI =
 ci: { 
     String urlJobName = auxiliary.getTopJobName(env.BUILD_URL)
 
-    def propertyList = ["compute-rocm-dkms-no-npi-hipclang":[pipelineTriggers([cron('0 1 * * 0')])]]
+    def propertyList = []
     propertyList = auxiliary.appendPropertyList(propertyList)
 
-    def jobNameList = ["compute-rocm-dkms-no-npi-hipclang":([ubuntu16:['gfx900'],centos7:['gfx906'],centos8:['gfx906'],sles15sp1:['gfx908']])]
+    def jobNameList = [:]
     jobNameList = auxiliary.appendJobNameList(jobNameList)
 
     propertyList.each 
@@ -72,9 +72,9 @@ ci: {
     // For url job names that are not listed by the jobNameList i.e. compute-rocm-dkms-no-npi-1901
     if(!jobNameList.keySet().contains(urlJobName))
     {
-        properties(auxiliary.addCommonProperties([pipelineTriggers([cron('0 1 * * *')])]))
+        properties(auxiliary.addCommonProperties([pipelineTriggers([cron('0 1 * * 6')])]))
         stage(urlJobName) {
-            runCI([ubuntu16:['gfx906']], urlJobName)
+            runCI(['ubuntu20-cuda11':['anycuda']], urlJobName)
         }
     }
 }
