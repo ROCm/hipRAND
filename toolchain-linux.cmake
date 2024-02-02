1296f54a8349a89187dee9795c03c821d07a6703
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2024-02-02 13:05:23 -0700

    Switch from using hipcc to clang on non-CUDA

--- toolchain-linux.cmake
+++ toolchain-linux.cmake
@@ -1,17 +1,19 @@
-#set(CMAKE_MAKE_PROGRAM "nmake.exe")
-#set(CMAKE_GENERATOR "Ninja")
-# Ninja doesn't support platform
-#set(CMAKE_GENERATOR_PLATFORM x64)
 
 if (DEFINED ENV{ROCM_PATH})
-  set(rocm_bin "$ENV{ROCM_PATH}/hip/bin")
+  set(rocm_bin "$ENV{ROCM_PATH}/bin")
 else()
-  set(rocm_bin "/opt/rocm/hip/bin")
+  set(ROCM_PATH "/opt/rocm" CACHE PATH "Path to the ROCm installation.")
+  set(rocm_bin "/opt/rocm/bin")
 endif()
 
+if (NOT DEFINED ENV{CXX})
+  set(CMAKE_CXX_COMPILER "${rocm_bin}/amdclang++")
+else()
+  set(CMAKE_CXX_COMPILER "$ENV{CXX}")
+endif()
 
-# set(CMAKE_CXX_COMPILER "hipcc")
-# set(CMAKE_C_COMPILER "hipcc")
-set(CMAKE_CXX_COMPILER "${rocm_bin}/hipcc")
-set(CMAKE_C_COMPILER "${rocm_bin}/hipcc")
-
+if (NOT DEFINED ENV{CC})
+  set(CMAKE_C_COMPILER "${rocm_bin}/amdclang")
+else()
+  set(CMAKE_C_COMPILER "$ENV{CC}")
+endif()
